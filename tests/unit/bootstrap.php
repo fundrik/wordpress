ce0c6a63e68de28ec61c05bc136629e0458a931f
Denis Yanchevskiy <denis@denisco.pro>
2026-02-28 00:59:16 +0300

    Updates

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-define( 'FUNDRIK_MAIN_FILE', realpath( __DIR__ . '/../fundrik.php' ) );
+define( 'FUNDRIK_MAIN_FILE', realpath( __DIR__ . '/../../fundrik.php' ) );
 
 define( 'FUNDRIK_PATH', realpath( dirname( FUNDRIK_MAIN_FILE ) ) . '/' );
 
@@ -10,7 +10,7 @@ define( 'FUNDRIK_URL', 'http://example.test/wp-content/plugins/fundrik/' );
 
 define( 'FUNDRIK_VERSION', '1.0.0' );
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
 define( 'ARRAY_A', 'ARRAY_A' );
